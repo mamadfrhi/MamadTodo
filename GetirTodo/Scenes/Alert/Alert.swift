d72e800515288f2d484903f0241a5af0312f4233
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-21 21:36:29 +0200

    fixed bug on Alert class

--- GetirTodo/Scenes/Alert/Alert.swift
+++ GetirTodo/Scenes/Alert/Alert.swift
@@ -9,16 +9,16 @@ import UIKit
 
 
 protocol Alert {
-    func showMessage(title: String, message: String, on: UIViewController)
+    func showMessage(title: String, message: String, on VC: UIViewController)
 }
 
 class AlertPresenter: Alert {
     
     static let shared = AlertPresenter()
     
-    func showMessage(title: String, message: String, on: UIViewController) {
+    func showMessage(title: String, message: String, on VC: UIViewController) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        alert.present(on, animated: true, completion: nil)
+        VC.present(alert, animated: true, completion: nil)
     }
 }
