4e872f3a135cbe400880d13fb9272ca9df1241b8
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-21 17:40:31 +0200

    Testing for create method of the CoreData implemented.

--- GetirTodoTests/GetirTodoTests.swift
+++ GetirTodoTests/GetirTodoTests.swift
@@ -8,26 +8,40 @@
 import XCTest
 @testable import GetirTodo
 
+/*
+ Here we avoid colliding with the app data by:
+ 
+ - creating a new CoreDataManagerTest which sets up it's own in-memory viewContext (mainthread)
+ - still doing everything on the main thread
+ 
+ */
+
 class GetirTodoTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var todoStorageManager: TodoStorageManager!
+    var coreDataStack: CoreDataTestStack!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        coreDataStack = CoreDataTestStack()
+        todoStorageManager = TodoStorageManager(mainContext: coreDataStack.mainContext)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_create_todo() {
+        // given
+        let todo = Todo(id: UUID().uuidString,
+                        title: "test todo title",
+                        description: "wonderful description",
+                        createdAt: Date())
+        todoStorageManager.create(object: todo) { (_) in }
+        
+        // when
+        todoStorageManager.fetch { (result) in
+            let todos = try! result.get()!
+            let todoManagedObjContainer = TodosManagedObjectsContainer(todosNSManagedObjects: todos)
+            let recentlyAddedtodo = todoManagedObjContainer.todos.first!
+            // then
+            XCTAssertEqual(recentlyAddedtodo.id, todo.id)
         }
     }
-
 }
