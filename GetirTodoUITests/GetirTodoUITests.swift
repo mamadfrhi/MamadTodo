659f343d9d3a15fb2192630a5667d4680dbc8277
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-21 22:13:35 +0200

    written 2 UI tests for AddPage(AddVC).

--- GetirTodoUITests/GetirTodoUITests.swift
+++ GetirTodoUITests/GetirTodoUITests.swift
@@ -8,35 +8,36 @@
 import XCTest
 
 class GetirTodoUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    private func testEmptyAddPage() {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.navigationBars["To Do List"].buttons["Add"].tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Save"]/*[[".buttons[\"Save\"].staticTexts[\"Save\"]",".staticTexts[\"Save\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let alert = app.alerts["Warning"]
+        XCTAssertTrue(alert.exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    private func testFilledAddPage() {
+        
+        let app = XCUIApplication()
+        app.navigationBars["To Do List"].buttons["Add"].tap()
+        app.textFields["Title"].tap()
+        var mainPageViewExist = app.navigationBars["To Do List"].exists
+        XCTAssertFalse(mainPageViewExist)
+        
+        // write a in titleTextField
+        let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        aKey.tap()
+        aKey.tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element.tap()
+        
+        // write B in descriptionTextView
+        let bKey = app/*@START_MENU_TOKEN@*/.keys["B"]/*[[".keyboards.keys[\"B\"]",".keys[\"B\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        bKey.tap()
+        bKey.tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Save"]/*[[".buttons[\"Save\"].staticTexts[\"Save\"]",".staticTexts[\"Save\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        mainPageViewExist = app.navigationBars["To Do List"].exists
+        XCTAssertTrue(mainPageViewExist)   
     }
 }
